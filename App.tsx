52276f15de5deb8bb4dae8dad452f8c6ab50a0ee
Gustavo Marques <gtooliveiramarques@gmail.com>
2022-06-23 14:42:53 -0300

    Image upload example on React Native and multpart API

--- App.tsx
+++ App.tsx
@@ -1,11 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import axios from "axios";
 
 export default function App() {
+  const uploadImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: false,
+    });
+    if (result.cancelled) return;
+    let localUri = result.uri;
+    let fileName = localUri.split("/").pop();
+    if (fileName !== undefined) {
+      let match = /\.(\w+)$/.exec(fileName);
+      let type = match ? `image/${match[1]}` : "image";
+      let formData = new FormData();
+      formData.append("file", {
+        // @ts-ignore
+        uri: localUri,
+        name: "newimage",
+        type: "image/jpg",
+      });
+
+      axios
+        .post(
+          "https://1txfn5rjle.execute-api.us-east-2.amazonaws.com/staging/users/upload-image",
+          formData,
+          {
+            headers: {
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoxLCJ1c2VyX3V1aWQiOiIyMGIwMTJiZS02ZmY0LTQwNjctOTc2NC05ZGJjZmNkN2ZlMDgiLCJpYXQiOjE2NTYwMDQxNzAsImV4cCI6MTY1NjA5MDU3MH0.KZVAzP9ZjkN0EckUTUG3F3JVSlp7yrx1KMI58fkC-GQ",
+              "content-type": "multipart/form-data",
+            },
+          }
+        )
+        .then((res) => {
+          console.log(res.data);
+        });
+    }
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Button onPress={uploadImage} title="UPLOAD" />
     </View>
   );
 }
@@ -13,8 +50,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
